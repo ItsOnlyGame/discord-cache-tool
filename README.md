878a190f2e1cfdec0452a0d6a905e2ff1e3180c7
ItsOnlyGame <33194188+ItsOnlyGame@users.noreply.github.com>
2023-01-19 23:58:16 +0200

    V3.0.0
    - Switched from Java to Rust

--- README.md
+++ README.md
@@ -2,11 +2,7 @@
 Gets every file from discords cache folder and changes the extension to the file type 
 
 ## Get started
-Just double click the program and it should create a folder with everything from the discord cache in the right format.
-If that doesn't work, open up a terminal and open the jar with this command
+Run the executable in terminal or double click it. 
+Download can be found on the right in releases. Make sure to download the latest version
 
-```
-java -jar Discord-Cache-Tool-2.0.0.jar
-```
-Remember to use Java 16+
 WINDOWS ONLY
---
+++ src/main.rs
@@ -0,0 +1,59 @@
+extern crate fs_extra;
+extern crate dirs;
+use std::{fs, path::Path};
+use fs_extra::{dir::CopyOptions, TransitProcess};
+
+fn main() {
+    // Default destination for modified discord cache files
+    let destination = String::from("./discord_cache");
+
+    // Create discord_cache directory, and remove it and its contents if exists
+    if Path::new(&destination).exists() {
+        fs::remove_dir_all(&destination).unwrap();
+    }
+    fs::create_dir(&destination).unwrap();
+    
+    let options = CopyOptions::new(); //Initialize default values for CopyOptions
+    let handle = |process_info: TransitProcess| {
+       println!("Copying files to ./discord_cache directory: {}", process_info.file_name);
+       fs_extra::dir::TransitProcessResult::ContinueOrAbort
+    };
+
+
+    let path_to_discord_cache = String::from(dirs::home_dir().unwrap().to_str().unwrap().to_owned() + "\\AppData\\Roaming\\discord\\Cache");
+
+    let mut from_paths = Vec::new();
+    for file in fs::read_dir(path_to_discord_cache).unwrap() {
+        let path = file.unwrap().path().to_str().unwrap().to_owned();
+        from_paths.push(path);
+    }
+
+    fs_extra::copy_items_with_progress(&from_paths, &destination, &options, handle).unwrap();
+
+    // Change file extensions to correct mime types
+    for file in fs::read_dir(&destination).unwrap() {
+        let entry = file.unwrap();
+        let path_buffer = entry.path();
+        let path = path_buffer.to_str().unwrap();
+
+        let kind = infer::get_from_path(path);
+
+        let kind = match kind {
+            Ok(type_option) => type_option,
+            Err(_error) => continue,
+        };
+
+        match kind {
+            Some(value) => {
+                let mime = value.mime_type().to_string();
+                let mime_array: Vec<&str> = mime.split("/").collect();
+
+                let together = format!("{}.{}", path, mime_array[1]);
+
+                fs::rename(path, together).unwrap();
+                println!("Adding ext to {}", entry.file_name().to_str().unwrap())
+            },
+            None => println!("Ignoring file: {} - No MIME type", entry.file_name().to_str().unwrap()),
+        }
+    }
+}
